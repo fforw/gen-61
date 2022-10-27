84f351d1d97ea6052f9a4fb078f659826c4fd9a9
Sven Helmberger <fforw@gmx.de>
2022-10-28 00:18:30 +0200

    Flowfield save point

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import domready from "domready"
 import "./style.css"
-import { randomPaletteWithBlack } from "./randomPalette"
 import Color, { getLuminance } from "./Color"
+
+import { voronoi } from "d3-voronoi"
 
 
 const PHI = (1 + Math.sqrt(5)) / 2;
@@ -29,11 +30,74 @@ function getColorExcluding(... exclusions)
     return color;
 }
 
+function drawPolygon(polygon, palette)
+{
+    const last = polygon.length - 1
+    const [x1, y1] = polygon[last]
+
+    ctx.beginPath()
+    ctx.moveTo(
+        x1 | 0,
+        y1 | 0
+    )
+
+    for (let i = 0; i < polygon.length; i++)
+    {
+        const [x1, y1] = polygon[i]
+        ctx.lineTo(x1 | 0, y1 | 0)
+    }
+    ctx.fill()
+    ctx.stroke()
+}
+
+
 /**
  * @type CanvasRenderingContext2D
  */
 let ctx;
 let canvas;
+
+const resolution = 80
+const FORCE_LEN = 32
+const BASE_FORCE = 4000000
+
+function drawArrow(x0, y0, x1, y1)
+{
+    const { width, height} = config;
+
+    const dy = y1 - y0;
+    const dx = x1 - x0;
+
+    if (dx * dx + dy * dy > 2)
+    {
+        const nx = dy * 0.08
+        const ny = -dx * 0.08
+
+        const start = 0.01
+        const end = 0.5
+
+        const x2 = x0 + (x1 - x0) * start
+        const y2 = y0 + (y1 - y0) * start
+        const x3 = x0 + (x1 - x0) * end
+        const y3 = y0 + (y1 - y0) * end
+
+        const x4 = x0 + (x1 - x0) * (start + (end - start) * 0.6)
+        const y4 = y0 + (y1 - y0) * (start + (end - start) * 0.6)
+
+        ctx.beginPath()
+        ctx.moveTo(x2, y2)
+        ctx.lineTo(x3, y3)
+
+        ctx.moveTo(x3, y3)
+        ctx.lineTo(x4 + nx, y4 + ny)
+        ctx.moveTo(x3, y3)
+        ctx.lineTo(x4 - nx, y4 - ny)
+        ctx.stroke()
+    }
+}
+
+
+const key = (x,y) => x + "/" + y
 
 domready(
     () => {
@@ -60,7 +124,7 @@ domready(
 
         const paint = () => {
 
-            const palette = randomPaletteWithBlack()
+            const palette = ["#454d66", "#309975", "#58b368", "#dad873", "#efeeb4"]//["#ffa822", "#134e6f", "#ff6150", "#1ac0c6", "#dee0e6"]//randomPaletteWithBlack()
 
             const bgColor = palette[0 | Math.random() * palette.length]
             config.palette = palette
@@ -78,6 +142,10 @@ domready(
 
             let area = (width * height) * (0.15 + 0.85 * Math.random() )
 
+            const pts = []
+            const forces = []
+
+            const sites = new Map()
             while (area > 0)
             {
                 const fillColor = getColorExcluding(bgColor, fgColor)
@@ -115,9 +183,108 @@ domready(
                 ctx.arc(x, y, +radius, 0, TAU, true)
                 ctx.fill()
 
+                const len = TAU * radius
+                const count = Math.floor( len/ resolution);
+                const step = TAU/count
+                angle = 0
+
+                const offset = Math.floor(Math.random() * 4) * TAU / 4
+
+                for (let i=0; i < count; i++)
+                {
+                    const sx = Math.round(x + Math.cos(angle) * radius)
+                    const sy = Math.round(y + Math.sin(angle) * radius)
+                    sites.set(key(sx,sy), pts.length)
+                    pts.push([
+                        sx,
+                        sy
+                    ])
+                    forces.push([
+                        Math.cos(angle + offset),
+                        Math.sin(angle + offset)
+                    ])
+
+                    angle += step
+                }
+
                 area -= Math.PI * radius * radius
 
             }
+            // console.log("SITES", sites)
+            // console.log("POINTS", pts)
+
+            const v = voronoi().extent([[0,0], [width, height]])
+            const diagram = v(pts)
+            // console.log("DIAGRAM", diagram)
+
+            const polygons = diagram.polygons()
+            ctx.strokeStyle = fgColor
+
+            //polygons.forEach( p => drawPolygon(p, config.palette))
+
+            for (let currentY = 0; currentY < height; currentY += FORCE_LEN* 0.4     )
+            {
+                for (let currentX = 0; currentX < width; currentX += FORCE_LEN * 0.4)
+                {
+                    const current = diagram.find(currentX,currentY)
+                    const index = sites.get(key(current[0],current[1]))
+                    const { halfedges } = diagram.cells[index]
+
+                    const nodes = [{site: current, index}]
+                    halfedges.forEach(
+                        e => {
+                            const { left, right } = diagram.edges[e]
+                            const other = current[0] === left[0] && current[1] === left[1] ? right : left
+                            if (other)
+                            {
+                                nodes.push(
+                                    {
+                                        site: other,
+                                        index: sites.get(key(other[0],other[1]))
+                                    }
+                                )
+                            }
+                        }
+                    )
+
+                    let dx = 0
+                    let dy = 0
+
+                    const influences = []
+                    nodes.forEach(
+                        ({site, index}) => {
+
+                            const [fx,fy] = forces[index]
+
+
+                            const x = currentX - site[0]
+                            const y = currentY - site[1]
+
+                            const influence = Math.min(1, BASE_FORCE/Math.pow(x*x+y*y, 2))
+                            dx += fx * influence
+                            dy += fy * influence
+
+                            influences.push(influence)
+                        }
+                    )
+                        console.log("INFLUENCES", influences)
+
+                    const f = 1/Math.sqrt(dx * dx + dy * dy)
+                    dx *= f
+                    dy *= f
+
+
+                    drawArrow( currentX, currentY,currentX + dx * FORCE_LEN, currentY + dy * FORCE_LEN)
+               }
+            }
+
+            // forces.forEach( (f,idx) => {
+            //
+            //     const [x,y] = pts[idx]
+            //     const [fx,fy] = forces[idx]
+            //
+            //     drawArrow( x, y,x + fx * FORCE_LEN, y + fy * FORCE_LEN)
+            // })
         }
 
         paint()
